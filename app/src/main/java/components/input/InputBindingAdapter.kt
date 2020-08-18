f1f5346766a3ea031318f6b6479ad3124648fffa
João Damazio <joaohd98@gmail.com>
2020-08-18 11:28:38 -0300

    Add on enter submit

--- app/src/main/java/components/input/InputBindingAdapter.kt
+++ app/src/main/java/components/input/InputBindingAdapter.kt
@@ -1,16 +1,27 @@
 package components.input
 
+import android.view.KeyEvent
+import android.view.View
 import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import com.joao.simsschool.R
 
 object InputBindingAdapter {
-    @JvmStatic @BindingAdapter("app:inputModel")
-    fun setViewModel(view: InputView, inputModel: InputModel) {
+    @JvmStatic @BindingAdapter(value = ["app:inputModel", "app:inputModelSubmit"], requireAll = false)
+    fun setViewModel(view: InputView, inputModel: InputModel, onSubmit: (() -> Unit)?) {
         view.binding.inputModel = inputModel
 
         val edit = view.findViewById<EditText>(R.id.view_input)
         edit.addTextChangedListener(inputModel.getChangeTextWatcher())
         edit.onFocusChangeListener = inputModel.getOnFocusChangeListener()
+
+        if(onSubmit != null) {
+            edit.setOnKeyListener { _, keyCode, event ->
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                    onSubmit()
+                }
+                false
+            }
+        }
     }
 }

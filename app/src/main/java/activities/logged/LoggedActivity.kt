f07f29130247e59552e2e3d5c27c6afdc5c894cb
João Damazio <joaohd98@gmail.com>
2020-08-20 17:17:16 -0300

    Finish main layout

--- app/src/main/java/activities/logged/LoggedActivity.kt
+++ app/src/main/java/activities/logged/LoggedActivity.kt
@@ -2,11 +2,7 @@ package activities.logged
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.viewpager.widget.ViewPager
-import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.tabs.TabLayout
 import com.joao.simsschool.R
 
@@ -16,9 +12,9 @@ class LoggedActivity : AppCompatActivity() {
         setContentView(R.layout.activity_logged)
 
         val sectionsPagerAdapter = LoggedPageAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val viewPager: ViewPager = findViewById(R.id.activity_logged_view_pager)
         viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
+        val tabs: TabLayout = findViewById(R.id.activity_logged_tabs)
         tabs.setupWithViewPager(viewPager)
 
     }

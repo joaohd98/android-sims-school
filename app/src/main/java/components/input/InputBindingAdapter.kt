cd59bafb7efcbe9d4cd5476aa9553a62c5f1ef26
João Damazio <joaohd98@gmail.com>
2020-08-20 13:41:17 -0300

    Remove warning

--- app/src/main/java/components/input/InputBindingAdapter.kt
+++ app/src/main/java/components/input/InputBindingAdapter.kt
@@ -6,22 +6,20 @@ import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import com.joao.simsschool.R
 
-object InputBindingAdapter {
-    @JvmStatic @BindingAdapter(value = ["app:inputModel", "app:inputModelSubmit"], requireAll = false)
-    fun setViewModel(view: InputView, inputModel: InputModel, onSubmit: (() -> Unit)?) {
-        view.binding.inputModel = inputModel
+@BindingAdapter(value = ["inputModel", "inputModelSubmit"], requireAll = false)
+fun setViewModel(view: InputView, inputModel: InputModel, onSubmit: (() -> Unit)?) {
+    view.binding.inputModel = inputModel
 
-        val edit = view.findViewById<EditText>(R.id.view_input)
-        edit.addTextChangedListener(inputModel.getChangeTextWatcher())
-        edit.onFocusChangeListener = inputModel.getOnFocusChangeListener()
+    val edit = view.findViewById<EditText>(R.id.view_input)
+    edit.addTextChangedListener(inputModel.getChangeTextWatcher())
+    edit.onFocusChangeListener = inputModel.getOnFocusChangeListener()
 
-        if(onSubmit != null) {
-            edit.setOnKeyListener { _, keyCode, event ->
-                if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
-                    onSubmit()
-                }
-                false
+    if(onSubmit != null) {
+        edit.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                onSubmit()
             }
+            false
         }
     }
 }

c66f09ce8f911a3c7077b0ebcc1464c39888125d
João Damazio <joaohd98@gmail.com>
2020-08-20 13:24:06 -0300

    Add part of bottom navigation

--- app/src/main/java/activities/LoggedActivity.kt
+++ app/src/main/java/activities/LoggedActivity.kt
@@ -1,14 +1,28 @@
 package activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.joao.simsschool.R
 
 class LoggedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.show()
-
         setContentView(R.layout.activity_logged)
+
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        val navController = findNavController(R.id.nav_host_fragment_logged)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_scores,
+            R.id.navigation_classes, R.id.navigation_tips,
+            R.id.navigation_menu
+        ))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }

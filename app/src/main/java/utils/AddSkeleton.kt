3026ec2c5348d0616884e26ca7722063296080ae
João Damazio <joaohd98@gmail.com>
2020-08-26 12:12:09 -0300

    Add skeleton

--- app/src/main/java/utils/AddSkeleton.kt
+++ app/src/main/java/utils/AddSkeleton.kt
@@ -1,19 +1,27 @@
 package utils
 
+import android.R.attr
+import android.util.Log
+import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
-import androidx.core.view.forEachIndexed
-import androidx.core.view.get
+import androidx.core.view.*
 import com.facebook.shimmer.ShimmerFrameLayout
 import com.joao.simsschool.R
 import kotlinx.android.synthetic.main.view_home_classes.view.*
 
+
 fun ViewGroup.addSkeletonAllElementsInner() {
     this.forEachIndexed { index, view ->
+        val layoutParams =  LinearLayout.LayoutParams(view.layoutParams)
+        layoutParams.setMargins(view.marginLeft, view.marginTop, view.marginRight, view.marginBottom)
+        layoutParams.gravity = (view.layoutParams as LinearLayout.LayoutParams).gravity
+
         val shimmer = ShimmerFrameLayout(context)
 
         shimmer.background = ContextCompat.getDrawable(context, R.drawable.skeleton)
-        val layoutParams = ViewGroup.LayoutParams(view.layoutParams)
         shimmer.layoutParams = layoutParams
 
         linear_layout.removeView(view)

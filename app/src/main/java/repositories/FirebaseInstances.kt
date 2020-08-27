e2599eb2ead7d6d60bf6b2e499e67023743ec222
João Damazio <joaohd98@gmail.com>
2020-08-27 11:06:41 -0300

    Add no internet connection error

--- app/src/main/java/repositories/FirebaseInstances.kt
+++ app/src/main/java/repositories/FirebaseInstances.kt
@@ -2,10 +2,19 @@ package repositories
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.storage.FirebaseStorage
 
 object FirebaseInstances {
     val auth: FirebaseAuth = FirebaseAuth.getInstance()
     val firestore = FirebaseFirestore.getInstance()
     val storage = FirebaseStorage.getInstance()
+
+    init {
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(false)
+            .build()
+
+        firestore.firestoreSettings = settings
+    }
 }

d8d804474d4774aedd52587de46ffd45c21eec83
João Damazio <joaohd98@gmail.com>
2020-08-31 12:18:52 -0300

    Fix scroll indicator

--- app/src/main/java/screens/logged/scores/ScoresScreen.kt
+++ app/src/main/java/screens/logged/scores/ScoresScreen.kt
@@ -1,18 +1,20 @@
 package screens.logged.scores
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.joao.simsschool.R
+import kotlinx.android.synthetic.main.screen_scores.view.*
 import kotlinx.android.synthetic.main.view_scores_classes.*
-import kotlinx.android.synthetic.main.view_scores_classes_card.*
 import kotlinx.android.synthetic.main.view_scores_semesters.*
-import kotlinx.android.synthetic.main.view_scores_semesters.view_scores_semesters_recycler_view
 import screens.logged.scores.components.ScoresClassesAdapter
 import screens.logged.scores.components.ScoresSemestersAdapter
 
@@ -57,9 +59,32 @@ class ScoresScreen : Fragment() {
     private fun initClasses() {
         val viewManager = LinearLayoutManager(requireContext())
         val viewAdapter = ScoresClassesAdapter(8)
+        var checkScrollingUp = true
+
         view_scores_classes_recycler_view.apply {
             layoutManager = viewManager
             adapter = viewAdapter
+
+            isNestedScrollingEnabled = false
+//            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+//                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//                    super.onScrolled(recyclerView, dx, dy)
+//                    val view = view_scores_semesters_recycler_view
+//                    val height = view.layoutManager?.height!!
+//
+//
+//                    Log.d("aaa", dy.toString())
+//                    Log.d("aaa", height.toString())
+//
+//                    if (dy > height) {
+//                        view.visibility = View.GONE
+//                    }
+//                    else {
+//                        view.visibility = View.VISIBLE
+//                    }
+//
+//                }
+//            })
         }
     }
 }

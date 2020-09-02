4bb3c253a0ed84e5e4ad70a8af75991761003976
João Damazio <joaohd98@gmail.com>
2020-09-02 16:47:19 -0300

    Add loading and error screen calendar

--- app/src/main/java/screens/logged/classes/ClassesScreen.kt
+++ app/src/main/java/screens/logged/classes/ClassesScreen.kt
@@ -7,6 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.joao.simsschool.R
+import components.error_view.OnTryAgainClickDataBinding
+import kotlinx.android.synthetic.main.fragment_classes.*
+import kotlinx.android.synthetic.main.screen_scores.*
 import repositories.RepositoryStatus
 import screens.logged.home.HomeViewModel
 import utils.observeOnce
@@ -24,6 +27,7 @@ class ClassesScreen : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         callRequests()
+        initErrorView()
         setObserves()
     }
 
@@ -39,17 +43,26 @@ class ClassesScreen : Fragment() {
         viewModel.calendarStatus.observe(viewLifecycleOwner) {
             when(it) {
                 RepositoryStatus.FAILED -> {
-
-                }
-                RepositoryStatus.LOADING -> {
-
+                    view_classes_view_switcher_error_loading.showNext()
                 }
                 RepositoryStatus.SUCCESS -> {
-
+                    view_classes_view_switcher.showNext()
                 }
                 else -> {}
             }
         }
+    }
+
+    private fun initErrorView() {
+        view_classes_error_view.setTryAgain(object: OnTryAgainClickDataBinding {
+            override fun showLoading() {
+                view_classes_view_switcher_error_loading.showNext()
+            }
+
+            override fun onClick() {
+                viewModel.callCalendar(true )
+            }
+        })
     }
 
 }

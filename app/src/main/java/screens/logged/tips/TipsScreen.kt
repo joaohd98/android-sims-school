aa04f4e40019f472c474000ecd2df5a4e81c8bc2
João Damazio <joaohd98@gmail.com>
2020-09-08 11:06:17 -0300

    Add part of status count indicator

--- app/src/main/java/screens/logged/tips/TipsScreen.kt
+++ app/src/main/java/screens/logged/tips/TipsScreen.kt
@@ -1,13 +1,20 @@
 package screens.logged.tips
 
+import android.content.Context
+import android.graphics.*
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.joao.simsschool.R
 import com.joao.simsschool.databinding.ScreenTipsBinding
+import utils.getPixels
+import kotlin.math.cos
+import kotlin.math.sin
 
 class TipsScreen : Fragment() {
     lateinit var binding: ScreenTipsBinding
@@ -26,6 +33,59 @@ class TipsScreen : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.imageView.clipToOutline = true
+        binding.screenTipsImage.clipToOutline = true
+    }
+}
+
+class CircleStatus: View {
+    constructor(context: Context) : super(context) { init() }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs){ init() }
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init()
+    }
+
+    private lateinit var circlePaint: Paint
+    private lateinit var separatorPaint: Paint
+
+    private fun init() {
+        circlePaint = Paint()
+        circlePaint.color = ContextCompat.getColor(context, R.color.blue)
+        circlePaint.isAntiAlias = true
+        circlePaint.style = Paint.Style.STROKE
+
+        separatorPaint = Paint()
+        separatorPaint.color = ContextCompat.getColor(context, R.color.background)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        val w = width.toFloat()
+        val h = height.toFloat()
+        val radiusPart = w / 2
+        val points = 2
+
+        val width = 10f
+        circlePaint.strokeWidth = width
+        separatorPaint.strokeWidth = width
+
+        canvas?.drawCircle(w / 2, h / 2, radiusPart - width, circlePaint)
+
+        val pointAngle = 360 / points
+        var angle = 0
+
+        while(angle < 360) {
+            val x = (cos(Math.toRadians(angle.toDouble())) * radiusPart).toFloat()
+            val y = (sin(Math.toRadians(angle.toDouble())) * radiusPart).toFloat()
+            canvas?.drawLine(radiusPart, radiusPart, x + radiusPart, y + radiusPart, separatorPaint)
+
+            angle += pointAngle
+        }
+
+        canvas?.rotate(180f)
     }
 }
--- app/src/main/java/utils/ContextPixels.kt
+++ app/src/main/java/utils/ContextPixels.kt
@@ -8,3 +8,10 @@ fun Context.getPixels(dp: Int): Int {
 
     return (dp * scale).toInt()
 }
+
+
+fun Context.getPixels(dp: Double): Int {
+    val scale = this.resources.displayMetrics.density
+
+    return (dp * scale).toInt()
+}

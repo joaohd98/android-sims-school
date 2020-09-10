3160eeef09d71ca9c39ee621b1d1f6d57881a190
João Damazio <joaohd98@gmail.com>
2020-09-10 01:14:38 -0300

    Add share to menu

--- app/src/main/java/screens/logged/menu/components/MenuItemView.kt
+++ app/src/main/java/screens/logged/menu/components/MenuItemView.kt
@@ -1,16 +1,17 @@
 package screens.logged.menu.components
 
+import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ShareCompat
 import com.joao.simsschool.R
-import com.joao.simsschool.databinding.ViewClassesCalendarBinding
 import com.joao.simsschool.databinding.ViewMenuItemBinding
-import kotlinx.android.synthetic.main.components_view_error.view.*
 
 class MenuItemView: ConstraintLayout {
     lateinit var binding: ViewMenuItemBinding
+    var typeButton: Int = -1
 
     constructor(context: Context) : super(context) {
         init(null, 0)
@@ -56,6 +57,47 @@ class MenuItemView: ConstraintLayout {
             R.styleable.MenuItemView_menu_text
         )
 
+        typeButton = typedArray.getInt(R.styleable.MenuItemView_menu_onPress, -1)
+
         typedArray.recycle()
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        binding.viewMenuInnerLinearLayout.setOnClickListener {
+            onCLick()
+        }
+    }
+
+    private fun onCLick() {
+        when(typeButton) {
+            0 -> onShare()
+            1 -> openMaps()
+            2 -> logout()
+            else -> {}
+        }
+    }
+
+    private fun onShare()  {
+        ShareCompat.IntentBuilder
+            .from(context as Activity)
+            .setText("""
+                Hey,
+
+			    Sims School is a simple Android APP, that I created with so much care
+
+			    See the source code at: https://github.com/joaohd98/android-sims-school
+            """.trimIndent())
+            .setType("text/plain")
+            .setChooserTitle("Sims School")
+            .startChooser()
+    }
+
+    private fun openMaps() {
+
+    }
+
+    private fun logout() {
+
     }
 }

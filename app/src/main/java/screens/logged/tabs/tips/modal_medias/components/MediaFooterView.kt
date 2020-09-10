fd5dcd6b27a640dde296ba4a0ab73517fd4c0ae7
João Damazio <joaohd98@gmail.com>
2020-09-10 17:26:43 -0300

    Add modal footer

---
+++ app/src/main/java/screens/logged/tabs/tips/modal_medias/components/MediaFooterView.kt
@@ -0,0 +1,36 @@
+package screens.logged.tabs.tips.modal_medias.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.joao.simsschool.R
+import com.joao.simsschool.databinding.ModalMediasItemFooterViewBinding
+import com.joao.simsschool.databinding.ModalMediasItemProgressViewBinding
+import com.joao.simsschool.databinding.ModalMediasItemTitleViewBinding
+
+class MediaFooterView: ConstraintLayout {
+    lateinit var binding: ModalMediasItemFooterViewBinding
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    )
+
+    init {
+        if (isInEditMode) {
+            LayoutInflater.from(context).inflate(R.layout.modal_medias_item_footer_view, this, true)
+        }
+        else {
+            binding = ModalMediasItemFooterViewBinding.inflate(LayoutInflater.from(context), this, true)
+        }
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+    }
+
+}

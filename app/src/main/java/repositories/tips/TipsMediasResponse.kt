5bc06b6412c8f3a66df580ce313c04bac31ae28d
João Damazio <joaohd98@gmail.com>
2020-09-16 11:40:47 -0300

    Remove image uri

--- app/src/main/java/repositories/tips/TipsMediasResponse.kt
+++ app/src/main/java/repositories/tips/TipsMediasResponse.kt
@@ -105,7 +105,8 @@ class TipsMediasResponse(
 
             try {
                 if(imageUri == null) {
-                    imageUri = FirebaseInstances.getURLFromMedia(image)!!
+                    imageUri = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"
+//                    imageUri = FirebaseInstances.getURLFromMedia(image)!!
                 }
 
                 imageBitmap = getBitmapFromURL(imageUri!!)
